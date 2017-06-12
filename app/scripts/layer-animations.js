aa3549152be34cdfd874e681e75d8a77cfd246e0
Carlos Coves Prieto <escorponox@gmail.com>
2017-06-12 11:53:06 +0200

    resize events

--- app/scripts/layer-animations.js
+++ app/scripts/layer-animations.js
@@ -1,89 +1,138 @@
-import { slide, calculateLayerHeight, calculateContainerHeight } from './layer-utils'
+import {
+  slide,
+  calculateLayerHeight,
+  calculateContainerHeight
+} from './layer-utils'
+import _ from 'lodash'
 
 const layers = Array.from(document.querySelectorAll('.c-layer'))
 const layerContainers = Array.from(document.querySelectorAll('.c-layers'))
 
 const staggeredBackwards = (backwardsLayers, previousLayer) => {
-
-  previousLayer = previousLayer || backwardsLayers[backwardsLayers.length - 1].previousElementSibling
+  previousLayer =
+    previousLayer ||
+    backwardsLayers[backwardsLayers.length - 1].previousElementSibling
   return previousLayer !== null
-    ? new Promise((resolve) => {
-      backwardsLayers.forEach((bLayer, index) => {
-        index === 0
-          ? bLayer.parentNode.insertBefore(bLayer, previousLayer)
-          : bLayer.parentNode.insertBefore(bLayer, backwardsLayers[index - 1])
+    ? new Promise(resolve => {
+        backwardsLayers.forEach((bLayer, index) => {
+          index === 0
+            ? bLayer.parentNode.insertBefore(bLayer, previousLayer)
+            : bLayer.parentNode.insertBefore(bLayer, backwardsLayers[index - 1])
+        })
+        setTimeout(() => {
+          resolve(
+            staggeredBackwards(
+              backwardsLayers,
+              backwardsLayers[backwardsLayers.length - 1].previousElementSibling
+            )
+          )
+        }, 200)
       })
-      setTimeout(() => {
-        resolve(staggeredBackwards(backwardsLayers, backwardsLayers[backwardsLayers.length - 1].previousElementSibling))
-      }, 200)
-    })
     : Promise.resolve(backwardsLayers)
 }
 
-const animateFrontLayers = layer => slide(layer.parentNode.lastElementChild, 480, 300)
-  .then(oldFrontLayer => new Promise((resolve) => {
-    oldFrontLayer.classList.add('c-layer--visited')
-    oldFrontLayer.classList.remove('c-layer--selected')
-    const backwardsLayers = [oldFrontLayer]
-    let previousLayer = oldFrontLayer.previousElementSibling
-    while (previousLayer !== layer) {
-      backwardsLayers.push(previousLayer)
-      previousLayer = previousLayer.previousElementSibling
-    }
-    setTimeout(() => {
-      resolve(backwardsLayers)
-    }, 150)
-  }))
-  .then(backwardsLayers => Promise.all(backwardsLayers.map((blayer, index) => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        blayer.classList.add('c-layer--hide')
-        resolve(blayer)
-      }, 150 * index)
-    })
-  })))
-  .then(data => new Promise(resolve => {
-    setTimeout(() => {
-      resolve(data)
-    }, 150)
-  }))
-  .then(backwardsLayers => new Promise((resolve) => {
-    staggeredBackwards(backwardsLayers, layer)
-      .then(backwardsLayers => {
-        resolve(layer)
-        return backwardsLayers
-      })
-      .then(backwardsLayers => backwardsLayers.forEach((blayer, index) => {
-        setTimeout(() => {
-          blayer.classList.remove('c-layer--hide')
-        }, 200 * (index + 1))
-      }))
-  }))
+const animateFrontLayers = layer =>
+  slide(layer.parentNode.lastElementChild, 480, 300)
+    .then(
+      oldFrontLayer =>
+        new Promise(resolve => {
+          oldFrontLayer.classList.add('c-layer--visited')
+          oldFrontLayer.classList.remove('c-layer--selected')
+          const backwardsLayers = [oldFrontLayer]
+          let previousLayer = oldFrontLayer.previousElementSibling
+          while (previousLayer !== layer) {
+            backwardsLayers.push(previousLayer)
+            previousLayer = previousLayer.previousElementSibling
+          }
+          setTimeout(() => {
+            resolve(backwardsLayers)
+          }, 150)
+        })
+    )
+    .then(backwardsLayers =>
+      Promise.all(
+        backwardsLayers.map((blayer, index) => {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              blayer.classList.add('c-layer--hide')
+              resolve(blayer)
+            }, 150 * index)
+          })
+        })
+      )
+    )
+    .then(
+      data =>
+        new Promise(resolve => {
+          setTimeout(() => {
+            resolve(data)
+          }, 150)
+        })
+    )
+    .then(
+      backwardsLayers =>
+        new Promise(resolve => {
+          staggeredBackwards(backwardsLayers, layer)
+            .then(backwardsLayers => {
+              resolve(layer)
+              return backwardsLayers
+            })
+            .then(backwardsLayers =>
+              backwardsLayers.forEach((blayer, index) => {
+                setTimeout(() => {
+                  blayer.classList.remove('c-layer--hide')
+                }, 200 * (index + 1))
+              })
+            )
+        })
+    )
 
 const animateNewFrontLayer = layer => {
   const frontHeight = calculateLayerHeight(layer)
   const containerHeight = calculateContainerHeight(layer)
   layer.classList.add('c-layer--selected')
-  return Promise.all([slide(layer, frontHeight, 500, true), slide(layer.parentNode, containerHeight, 500, true)])
+  return Promise.all([
+    slide(layer, frontHeight, 500, true),
+    slide(layer.parentNode, containerHeight, 500, true)
+  ])
 }
 
 const resizeFrontLayer = event => {
   const target = event.target
-  if (target.classList.contains('evo_c-3dlayer--selected')) {
+  if (target.classList.contains('c-layer--selected')) {
     const frontHeight = calculateLayerHeight(target)
     const containerHeight = calculateContainerHeight(target)
-    return Promise.all([slide(target, frontHeight, 500, true), slide(target.parentNode, containerHeight, 500, true)])
+    return Promise.all([
+      slide(target, frontHeight, 500, true),
+      slide(target.parentNode, containerHeight, 500, true)
+    ])
   }
 }
 
-const moveForward = layer => {
-  layers.forEach(label => label.removeEventListener('click', moveForwardListener))
+const resizeAllFrontLayers = _.debounce(() => {
 
-  animateFrontLayers(layer)
-    .then(animateNewFrontLayer)
-    .then(() => {
-      layers.forEach(label => label.addEventListener('click', moveForwardListener))
-    })
+  const frontLayers = Array.from(
+    document.querySelectorAll('.c-layer--selected')
+  )
+
+  frontLayers.forEach(layer => {
+    const frontHeight = calculateLayerHeight(layer)
+    const containerHeight = calculateContainerHeight(layer)
+    slide(layer, frontHeight, 500, true)
+    slide(layer.parentNode, containerHeight, 500, true)
+  })
+}, 150)
+
+const moveForward = layer => {
+  layers.forEach(label =>
+    label.removeEventListener('click', moveForwardListener)
+  )
+
+  animateFrontLayers(layer).then(animateNewFrontLayer).then(() => {
+    layers.forEach(label =>
+      label.addEventListener('click', moveForwardListener)
+    )
+  })
 }
 
 const moveForwardListener = event => {
@@ -97,6 +146,7 @@ export default () => {
   layers.forEach(layer => {
     layer.addEventListener('click', moveForwardListener)
     layer.addEventListener('resizelayer', resizeFrontLayer)
+    window.addEventListener('resize', resizeAllFrontLayers)
   })
   layerContainers.forEach(layerContainer => {
     const selected = layerContainer.querySelector('.c-layer--selected')
